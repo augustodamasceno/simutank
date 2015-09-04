db8080fdd4ffbcdc811c21cf3256bac4f9b5b6ed
Augusto Matheus <augustomatheuss@iMac2-lab10.local>
2015-09-04 18:44:02 -0300

    begin

---
+++ simutank.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+##
+#  Simulator for Quanser's Coupled Tanks
+#  Copyright (C) 2015, Augusto Damasceno
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+import socket
+
+# REQread0 = 'READ 0'
+# REQread1 = 'READ 1'
+# REQwrite = 'WRITE %d'
+
+#receive 'WRITE %d %d\n' % (channel, volt)
+#ack 'ACK' or other
+
+
+
