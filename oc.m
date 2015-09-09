86b4aadfffeb277bffdd66dc59870977373bdb32
Augusto Damasceno <augustomatheuss@yahoo.com.br>
2015-09-09 12:57:19 -0300

    Delete oc.m

--- oc.m
+++
@@ -1,8 +0,0 @@
-constantAD = 1024/4.8;
-tank1 = dlmread('logOutput1');
-tank1 = tank1./constantAD;  
-
-plot(tank1,'linewidth',2);
-title('Level Tank 1(cm) x time(cycles)','fontsize', 14)
-xlabel('time(cycles)','fontsize', 12)
-ylabel('Level(cm)','fontsize', 12)
